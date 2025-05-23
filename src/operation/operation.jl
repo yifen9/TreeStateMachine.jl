8f86db94075a5f3cf6cbb09334c1d0b7fc9d3080
Li Yifeng <yifen9@yandex.com>
2025-05-23 22:36:18 +0200

    Update operation.jl

--- src/operation/operation.jl
+++ src/operation/operation.jl
@@ -7,15 +7,15 @@ using ..Engine
 const REGISTRY = Dict{Symbol, Function}()
 const DIR_CORE = joinpath(@__DIR__, "core")
 
-_exist(name::Symbol) = haskey(REGISTRY, name)
+_exist(name::Symbol)::Bool = haskey(REGISTRY, name)
 
-function set!(name::Symbol, fn::Function)
+function set!(name::Symbol, fn::Function)::Nothing
     _exist(name) && @warn "Operation `$(name)` overrided"
     REGISTRY[name] = fn
     return nothing
 end
 
-function get_function(name::Symbol)
+function get_function(name::Symbol)::Function
     if(_exist(name))
         return REGISTRY[name]
     else
@@ -23,11 +23,11 @@ function get_function(name::Symbol)
     end
 end
 
-function get_name_list()
+function get_name_list()::Vector{Symbol}
     return collect(keys(REGISTRY))
 end
 
-function _load()
+function _load()::Nothing
     for file in readdir(DIR_CORE)
         endswith(file, ".jl") && include(joinpath(DIR_CORE, file))
     end
